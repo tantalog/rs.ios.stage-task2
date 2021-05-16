00199fb377db92930b59d54b803a100ab3fbcc72
tantalog <kushnerovanton@gmail.com>
2021-05-16 22:45:22 +0300

    feat: Ex3 and Ex4 complete

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,26 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    NSMutableString *result = [NSMutableString stringWithString:@""];
+    if (numbersArray.count > 0) {
+        for (int i=0; i<4; i++) {
+            if (i < numbersArray.count) {
+                if ([numbersArray[i] integerValue] < 0) {
+                    return @"Negative numbers are not valid for input.";
+                }
+                if ([numbersArray[i] integerValue] > 255) {
+                    return @"The numbers in the input array can be in the range from 0 to 255.";
+                }
+                [result appendFormat:@"%@.", numbersArray[i]];
+            } else {
+                [result appendString:@"0."];
+            }
+        }
+        [result deleteCharactersInRange:NSMakeRange(result.length-1, 1)];
+        
+        return result;
+    }
+    return @"";
 }
 
 @end
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,34 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if (array == nil || array.count == 0 || ![array[0] isKindOfClass:NSArray.class]){
+        return @[];
+    }
+    
+    NSMutableArray *numbersArray = [NSMutableArray new];
+    NSMutableArray *stringsArray = [NSMutableArray new];
+    NSMutableArray *fullArray = [NSMutableArray new];
+    
+    for (NSArray *subArray in array) {
+        if ([subArray.firstObject isKindOfClass:NSNumber.class]) {
+            [numbersArray addObjectsFromArray:(subArray)];
+            [numbersArray sortUsingSelector:@selector(compare:)];
+        } else if ([subArray.firstObject isKindOfClass:NSString.class]) {
+            [stringsArray addObjectsFromArray:(subArray)];
+            [stringsArray sortUsingSelector:@selector(compare:)];
+        }
+    }
+    
+    if (numbersArray.count == 0) {
+        [fullArray addObjectsFromArray:stringsArray];
+    } else if (stringsArray.count == 0) {
+        [fullArray addObjectsFromArray:numbersArray];
+    } else {
+        [fullArray addObject:numbersArray];
+        [fullArray addObject:[stringsArray reverseObjectEnumerator].allObjects];
+    }
+    
+    return fullArray;
 }
 
 @end
