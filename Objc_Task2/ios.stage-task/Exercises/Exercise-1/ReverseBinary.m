5cf00806b29bdb025287081165353e0b166cac4e
tantalog <kushnerovanton@gmail.com>
2021-05-16 22:20:58 +0300

    feat: Ex1 complete and Ex2 complete

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,14 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    int  result = 0;
+    int  byte_len = 8;
+    
+    while (byte_len--) {
+        result = (result << 1) | (n & 1);
+        n >>= 1;
+    }
+    return result;
 }
+
+
--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,14 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    int pairsCount = 0;
+    for (int i = 0; i < array.count; i++)
+        for (int j = i + 1; j < array.count; j++) {
+            if (ABS(array[i].intValue - array[j].intValue) == number.intValue) {
+                pairsCount++;
+            }
+        }
+    return pairsCount;
 }
 
 @end
